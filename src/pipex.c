33dd21cbe8ba912c764a9bdbe3e0e25684abe91e
Edmund <50744370+yoyedmundyoy@users.noreply.github.com>
2024-07-13 12:58:59 +0000

    changes

--- src/pipex.c
+++ src/pipex.c
@@ -1,37 +1,61 @@
 #include "pipex.h"
 
 #include <stdio.h>
+#include<sys/wait.h>
 
 void exit_message(int exit_code)
 {
 	// If i = 1, print exit message
 	if (exit_code)
-	
-	// Exit
+		printf("Error message\n");
+	exit(0);
 }
 
 void execute(char* cmd, char *envp[])
 {
+	if (cmd || envp)
+		ft_putstr_fd("execute", 2);
 	// get path
 	// get l_cmd
 	// execve(path, l_cmd, envp)
 	// if execve fails, handle error, print error message, free and exit
 }
 
+int try_open(char *argv[], int in_out)
+{
+	int f;
+
+	if (!in_out)
+		f = open(argv[1], O_RDONLY);
+	else
+		f = open(argv[4], O_CREAT | O_TRUNC | O_RDWR);
+	if (f < 0)
+		exit(0);
+	return (f);
+}
+
 void child(char *argv[], char *envp[], int p_fd[])
 {
-	// Try to open the in file
-	// dup2 so infile becomes fd stdin 0
-	// dup2 so p_fd[1] (write end) becomes stdout 1
-	// execute function
+	int f;
+
+	f = try_open(argv, 1);
+	if (dup2(f, 1) < 0 || dup2(p_fd[0], 0) < 0)
+		exit(0);
+	close(p_fd[1]);
+	close(f);
+	execute(argv[3], envp);
 }
 
 void parent(char *argv[], char *envp[], int p_fd[])
 {
-	// Try to open the in file
-	// dup2 so infile becomes fd stdin 0
-	// dup2 so p_fd[1] (write end) becomes stdout 1
-	// execute function
+	int f;
+
+	f = try_open(argv, 0);
+	if (dup2(f, 0) < 0 || dup2(p_fd[1], 1) < 0)
+		exit(0);
+	close(p_fd[0]);
+	close(f);
+	execute(argv[2], envp);
 }
 
 int main(int argc, char *argv[], char *envp[])
@@ -44,7 +68,16 @@ int main(int argc, char *argv[], char *envp[])
 	if (argc != 5)
 		exit_message(1);
 	// Create pipe
-	// If pipe creation fails exit
+	if (pipe(p_fd) < 0)
+		exit(1);
+	pid = fork();
+	if (pid < 0)
+		exit(1);
+	if (!pid)
+		child(argv, envp, p_fd);
+	wait(NULL);
+	parent(argv, envp, p_fd);
+
 	// Fork
 	// If child process, call child function
 	// child(argv, envp, p_fd)
